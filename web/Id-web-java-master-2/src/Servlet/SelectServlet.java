94c6a693c03b855677f4753bd5ae43ea547adc35
porjulio <kevincezar28@gmail.com>
2021-11-27 12:56:31 +0800

    commit

--- web/Id-web-java-master-2/src/Servlet/SelectServlet.java
+++ web/Id-web-java-master-2/src/Servlet/SelectServlet.java
@@ -26,7 +26,13 @@ public class SelectServlet extends HttpServlet {
 		out.println("<html>");
 		out.println("<head>");
 		out.println("<title>Display User Details</title>");
-		out.println(" <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link rel=\\\"stylesheet\\\" href=\\\"https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css\\\"><script src=\\\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\\\"></script><script src=\\\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js\\\"></script><script src=\\\"https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js\\\"></script></script>");
+//		out.println(" <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link rel=\\\"stylesheet\\\" href=\\\"https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css\\\"><script src=\\\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\\\"></script><script src=\\\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js\\\"></script><script src=\\\"https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js\\\"></script></script>");
+		out.println("<meta charset=\"utf-8\">\r\n" + 
+				"  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\r\n" + 
+				"  <link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css\">\r\n" + 
+				"  <script src=\"https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js\"></script>\r\n" + 
+				"  <script src=\"https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js\"></script>\r\n" + 
+				"  <script src=\"https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js\"></script>");
 		out.println("</head>");
 		out.println("<body>");
 		out.println(" <h1>List of Users</h1>");
